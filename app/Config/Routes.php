0dd3856ccfb459bc5b525342ec1b256763372714
Juan Victor <juanvictoroliveira2009@gmail.com>
2020-03-05 11:34:06 -0300

    Adicionando as routas - ion_auth

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,12 +1,13 @@
-<?php namespace Config;
+<?php
+
+namespace Config;
 
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes(true);
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
-{
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
 	require SYSTEMPATH . 'Config/Routes.php';
 }
 
@@ -30,8 +31,14 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+//$routes->get('/', 'Home::index');
 
+$routes->group('auth', ['namespace' => 'IonAuth\Controllers'], function ($routes) {
+	$routes->get('/', 'Auth::index');
+	$routes->add('login', 'Auth::login');
+	$routes->get('logout', 'Auth::logout');
+	$routes->add('forgot_password', 'Auth::forgot_password');
+});
 /**
  * --------------------------------------------------------------------
  * Additional Routing
@@ -45,7 +52,6 @@ $routes->get('/', 'Home::index');
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
-{
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
